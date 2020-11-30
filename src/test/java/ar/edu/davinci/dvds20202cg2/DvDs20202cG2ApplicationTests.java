83a93302d7a0855e8159f8f5c0b1a70d2482829a
nzalazar92 <nico.zalazar92@gmail.com>
2020-11-30 17:19:59 -0300

    Se elimina proyecto uml y se sube el proyecto de spring
    Se elimina el proyecto uml porque se va agregar al proyecto spring

---
+++ src/test/java/ar/edu/davinci/dvds20202cg2/DvDs20202cG2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20202cg2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvDs20202cG2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
